b6678d4442dc9e8bcef918a431407b0e9644f64c
El-Moutaouakkil <el.moutaouakkil@hotmail.com>
2021-11-24 00:46:31 +0100

    MainBackground component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
+import MainBackground from "./components/MainBackground";
 
 function App() {
     return <div className='App'>
-      
+      <MainBackground />
     </div>;
 }
 
---
+++ src/components/MainBackground.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+function MainBackground() {
+    return (
+        <BgContainer>
+            <img src='/assets/map.jpg' alt='main background' />
+            
+        </BgContainer>
+    );
+}
+
+export default MainBackground;
+
+const BgContainer = styled.div`
+    img {
+       
+    }
+   
+`;
