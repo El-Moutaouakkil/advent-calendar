1f8f6cd46f0d668c44c400b546f409c5a2789661
El-Moutaouakkil <el.moutaouakkil@hotmail.com>
2021-11-25 01:09:30 +0100

    MainBackground cover on large display and Scrolling on small devices Completed

--- src/components/MainBackground.js
+++ src/components/MainBackground.js
@@ -5,7 +5,7 @@ function MainBackground() {
     return (
         <BgContainer>
             <img src='/assets/map.jpg' alt='main background' />
-            
+            <img src='/assets/fontana2.gif' alt='fontana' className='fontana' />
         </BgContainer>
     );
 }
@@ -13,8 +13,23 @@ function MainBackground() {
 export default MainBackground;
 
 const BgContainer = styled.div`
+    height: 100vh;
+    position: relative;
     img {
-       
+        width: 700px;
+        display: block;
     }
-   
+
+    .fontana {
+        position: absolute;
+        top: 0;
+        left: 0;
+        /* border : 2px solid orange; */
+    }
+    @media all and (min-width: 469px) {
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
 `;
